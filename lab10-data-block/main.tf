c7decf35e3242a0c753c0ed118b08e0cc79adddd
Michele Costa Christianson <michpimentel22@gmail.com>
2024-02-14 12:42:16 -0700

    update project

--- lab10-data-block/main.tf
+++ lab10-data-block/main.tf
@@ -33,3 +33,20 @@ resource "aws_instance" "app_server" {
     Name = "my-ec2-instance"
   }
 }
+
+
+data "aws_ami" "ubuntu" {
+  owners = ["099720109477"]
+
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
---
+++ terraform-project/main.tf
@@ -0,0 +1,37 @@
+# Resource block - EC2 instance
+resource "aws_instance" "pipeline" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name      = "tfproject"
+#   security_groups = [aws_security_group.jenkins_sg.id]
+
+  tags = {
+    Name = "jenkins-instance"
+  }
+}
+
+# Resource block - Security Group 
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Security Group for Jenkins Instance"
+  vpc_id      = "vpc id" # Deafult VPC
+
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["my-ip"]  # My IP address
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "jenkins_sg"
+  }
+}
